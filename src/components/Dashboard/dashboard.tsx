e97e4de149af9e8aa885cdd28d21ac9686560e3f
Andreysson-Araujo <andreyssonaraujo@gmail.com>
2023-07-25 08:20:07 -0500

    inserindo conexão com o bd

--- src/components/Dashboard/dashboard.tsx
+++ src/components/Dashboard/dashboard.tsx
@@ -1,55 +1,105 @@
+import { useEffect, useState } from "react";
+import api from "@/services/api";
 import { Flex, SimpleGrid, Text, Box } from "@chakra-ui/react";
 import dynamic from "next/dynamic";
-import { useEffect, useState } from "react";
 
-interface listaAtendimentos {
-    ano:number;
+interface AtendimentoData {
+  id: number;
+  comentarios: string;
+  created_at: string;
+  data_de_Atendimento: string;
+  quantidade: number;
+  servicos_id: number;
+  unidades_id: number;
+  updated_at: string;
+  usuarios_id: number;
+}
+
+export interface UnidadeData {
+  id: number;
+  nome: string;
+  // Outras propriedades da unidade, se houver
 }
 
 const Chart = dynamic(() => import("react-apexcharts"), {
-    ssr: false,
+  ssr: false,
 });
 
-const options = {
-    labels: ["Rio Branco", "Cruzeiro", "Xapuri"],
-    legend: {
-        position: "left",
-        markers: {
-            width: "20px",
-            shape: "square",
-            radius: 4,
-        }
-    }
-};
-
-const series = [300, 25, 50];
-
 export default function Dashboard() {
-    const [ listaAdtendimentosAno, setListaAdtendimentosAno ] = useState<listaAtendimentos[]>[[]]; 
+  const [listaAtendimentosAno, setListaAtendimentosAno] = useState<AtendimentoData[]>([]);
+  const [seriesData, setSeriesData] = useState<number[]>([]);
+  const [labelsData, setLabelsData] = useState<string[]>([]);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const responseAtendimentos = await api.get("/atendimentos"); // Substitua "/atendimentos" pelo endpoint correto da sua API
+        setListaAtendimentosAno(responseAtendimentos.data);
 
-    
+        // Puxar os dados da rota de unidades
+        const responseUnidades = await api.get("/unidades"); // Substitua "/unidades" pelo endpoint correto da sua API
+        const unidadesData: UnidadeData[] = responseUnidades.data;
 
-    return (
-        <>
-        
-        <Flex direction="column" h="100vh">
-            <SimpleGrid flex={1} gap={4} minChildWidth="320px" alignItems="flex-start">
-                <Box p={8} bg="gray.100" borderRadius={8} pb={4}>
-                    <Text fontSize="2xl" mb={4}>
-                        Todas as Centrais
-                    </Text>
-                    <Chart options={options} series={series} type="pie" height={300} />                
-                </Box>
-                <Box p={8} bg="gray.100" borderRadius={8} pb={4}>
-                    <Text fontSize="2xl" mb={4}>
-                        Estatística de atendimento
-                        <p>estatistica1</p>
-                        <p>estatistica2</p>
-                    </Text>
-                </Box>
-            </SimpleGrid>
-        </Flex>
-        </>
-    );
+        // Cria um objeto para contar a quantidade de vezes que cada unidade aparece
+        const unidadeCount: { [key: number]: number } = {};
+        responseAtendimentos.data.forEach((atendimento: AtendimentoData) => {
+          const unidadeId = atendimento.unidades_id;
+          unidadeCount[unidadeId] = (unidadeCount[unidadeId] || 0) + 1;
+        });
+
+        // Obtém as labels e séries para o gráfico de pizza
+        const unidadesIds = Object.keys(unidadeCount).map(Number);
+        const unidadesQuantidades = Object.values(unidadeCount);
+
+        // Preenche as labels com os nomes das unidades obtidos da rota de unidades
+        const unidadesNomes = unidadesIds.map((id) => {
+          const unidade = unidadesData.find((unidade) => unidade.id === id);
+          return unidade ? unidade.nome : `Unidade ${id}`;
+        });
+
+        setLabelsData(unidadesNomes);
+        setSeriesData(unidadesQuantidades);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const options = {
+    labels: labelsData,
+    legend: {
+      position: "left",
+      markers: {
+        width: "20px",
+        shape: "square",
+        radius: 4,
+      },
+    },
+  };
+
+  const series = seriesData;
+
+  return (
+    <>
+      <Flex direction="column" h="100vh">
+        <SimpleGrid flex={1} gap={4} minChildWidth="320px" alignItems="flex-start">
+          <Box p={8} bg="gray.100" borderRadius={8} pb={4}>
+            <Text fontSize="2xl" mb={4}>
+              Todas as Centrais
+            </Text>
+            <Chart options={options} series={series} type="pie" height={300} />
+          </Box>
+          <Box p={8} bg="gray.100" borderRadius={8} pb={4}>
+            <Text fontSize="2xl" mb={4}>
+              Estatística de atendimento
+              <p>estatistica1</p>
+              <p>estatistica2</p>
+            </Text>
+          </Box>
+        </SimpleGrid>
+      </Flex>
+    </>
+  );
 }
--- src/components/Filter/filter.tsx
+++ src/components/Filter/filter.tsx
@@ -1,37 +1,50 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Flex } from '@chakra-ui/react';
 import Select from 'react-select';
+import api from '@/services/api';
+import { UnidadeData } from '../Dashboard/dashboard';
 
 const options1 = [
   { value: '1', label: 'Geral' },
   { value: '2', label: 'Unidade' },
 ];
 
-const options2 = [
-  { value: '1', label: 'Todos' },
-  { value: '2', label: 'Rio branco' },
-  { value: '3', label: 'Cruzeiro do Sul' },
-  { value: '4', label: 'Xapuri' },
-  { value: '5', label: 'Brasileia' },
-  { value: '6', label: 'Movel' },
-];
-
 function Filter() {
-  const [selectedOption1, setSelectedOption1] = useState(null);
+  const [selectedOption1, setSelectedOption1] = useState<{ value: string; label: string } | null>(null);
+  const [selectedOption2, setSelectedOption2] = useState<{ value: string; label: string } | null>(null);
+  const [unidadesData, setUnidadesData] = useState<UnidadeData[]>([]);
 
-  const handleOption1Change = (selectedOption) => {
+  const handleOption1Change = async (selectedOption: { value: string; label: string } | null) => {
     setSelectedOption1(selectedOption);
+
+    // Se a opção selecionada for "Unidade", faz a requisição para a rota de unidades
+    if (selectedOption && selectedOption.value === '2') {
+      try {
+        const response = await api.get('/unidades'); // Substitua "/unidades" pelo endpoint correto da sua API
+        setUnidadesData(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
+  const handleOption2Change = (selectedOption: { value: string; label: string } | null) => {
+    setSelectedOption2(selectedOption);
+  };
+
+  // Função para obter as opções corretas para o segundo Select
+  const getUnidadesOptions = () => {
+    return unidadesData.map((unidade) => ({ value: unidade.id.toString(), label: unidade.nome }));
   };
 
   return (
     <Flex direction="row" alignItems="center" justifyContent="center">
       <Select options={options1} value={selectedOption1} onChange={handleOption1Change} />
 
-      { selectedOption1 && selectedOption1.value === '2' && (
-        <Select options={options2} />
-      )
-      }
-
+      {selectedOption1 && selectedOption1.value === '2' && (
+        <Select options={getUnidadesOptions()} value={selectedOption2} onChange={handleOption2Change} />
+      )}
+      
       <br />
       2020 / 2021 / 2022
     </Flex>
