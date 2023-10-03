e9a40e79b08a41d0f047b05c8132e4685c3c9b1a
andreysson-araujo <andreyssonaraujo@gmail.com>
2023-10-03 10:10:32 -0500

    deixando formulario dinamico

--- src/pages/ad/cadastro/index.tsx
+++ src/pages/ad/cadastro/index.tsx
@@ -7,15 +7,34 @@ import { error } from "console";
 import api from "@/services/api";
 import { useEffect, useState } from "react";
 import { Unidade } from "@/components/CriacaoDashboard/interfaces/UnidadeInterface";
+import { format } from 'date-fns';
+import { AtendimentoData } from "@/components/CriacaoDashboard/interfaces/AtendimentoInterface"; 
 
 
 export default function Formulario() {
   const [unidades, setUnidades] = useState<Unidade[]>([]); // Inicialize o estado com um array de Unidades
   const [selectedUnidade, setSelectedUnidade] = useState<string>(""); // Inicialize com uma string vazia
-  
+  const [servicos, setServicos] = useState<any[]>([])
+  const [dataAtendimento, setDataAtendimento] = useState<string>(getCurrentDate()); // Defina a data atual como valor inicial
+
+  function getCurrentDate() {
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = String(now.getMonth() + 1).padStart(2, "0");
+    const day = String(now.getDate()).padStart(2, "0");
+    return `${year}-${month}-${day}`;
+  }
+
   const exibirMSG = () => {
-    const mensagem = "Dados Inseridos"
-    alert(mensagem)
+    const currentDate = getCurrentDate();
+    
+    if (dataAtendimento > currentDate) {
+      alert("Erro: Você não pode cadastrar uma data futura.");
+      return;
+      
+    } else {
+      alert("Dados Inseridos");
+    }
   }
 
   useEffect(() => {
@@ -32,8 +51,22 @@ export default function Formulario() {
         console.error(error);
       }
     }
+    async function fetchServicos() {
+    try{
+
+      const response = await api.get("/servicos");
+      const data = response.data;
+
+      if (data && data.length > 0) {
+        setServicos(data);
+      }
+    } catch (error){
+      console.error(error)
+    }
+  }
 
     fetchUnidades();
+    fetchServicos()
   }, []);
 
   return (
@@ -58,41 +91,28 @@ export default function Formulario() {
       <Text fontSize={"1.2rem"} fontWeight={"800"} fontStyle={"italic"}>
         Data de Atendimento
       </Text>
-      <Input type={"date"} textAlign={"center"} w={800}  margin={"auto"} bg={"#fffffff"} />
+      <Input type={"date"} textAlign={"center"} w={800}  margin={"auto"} bg={"#fffffff"} value={dataAtendimento} onChange={(e) => setDataAtendimento(e.target.value)}/>
     </Box>
-    <Box marginTop={"2rem"} textAlign={"center"}>
+    {servicos.map((servico, index) => (
+    <Box key={index} marginTop={"2rem"} textAlign={"center"}>
       <Text fontSize={"1.2rem"} fontWeight={"800"} fontStyle={"italic"}>
-        Atendimento Chat
+        {servico.nome }
       </Text>
-      <Input type={"number"}  textAlign={"center"} w={800} margin={"auto"} bg={"#fffffff"} placeholder={"Quantos atendimentos de Chat ?"}/>
+      <Input type={"number"} textAlign={"center"} w={800} margin={"auto"} bg={"#fffffff"} placeholder={`Quantos atendimentos de ${servico.nome} ?`}/>
     </Box>
-    <Box marginTop={"2rem"} textAlign={"center"}>
-      <Text fontSize={"1.2rem"} fontWeight={"800"} fontStyle={"italic"}>
-        Atendimento Presenciais
-      </Text>
-      <Input type={"number"} textAlign={"center"} w={800} margin={"auto"} bg={"#fffffff"} placeholder={"Quantos atendimentos Presenciais ?"}/>
-    </Box>
-    <Box marginTop={"2rem"} textAlign={"center"}>
-      <Text fontSize={"1.2rem"} fontWeight={"800"} fontStyle={"italic"}>
-        Atendimento Call Center
-      </Text>
-      <Input type={"number"} textAlign={"center"} w={800} margin={"auto"} bg={"#fffffff"} placeholder={"Quantos atendimentos de Call Center ?"}/>
-    </Box>
-    <Box marginTop={"2rem"} textAlign={"center"}>
-      <Text fontSize={"1.2rem"} fontWeight={"800"} fontStyle={"italic"}>
-        Atendimento Redes Sociais
-      </Text>
-      <Input type={"number"} textAlign={"center"} w={800} margin={"auto"} bg={"#fffffff"} placeholder={"Quantos atendimentos de Redes Sociais ?"}/>
-    </Box>
+    ))}
     <Box marginTop={"2rem"} textAlign={"center"}>
       <Text fontSize={"1.2rem"} fontWeight={"800"} fontStyle={"italic"}>
         Comentarios
       </Text>
       <Input as={"textarea"} textAlign={"center"} w={800} h={40} margin={"auto"} bg={"#fffffff"} placeholder={"Algum Comentario"}/>
     </Box>
-    <Box marginTop={"2rem"} textAlign={"center"}>
-    <Link margin={"auto"} textAlign={"center"} href="cadastro/form/" p={3} bgColor={"green.400"} color={"#FFFFFF"} fontWeight={1000} onClick={exibirMSG}>
+    <Box margin={600} display={"flex"}   marginTop={"2rem"} textAlign={"center"} justifyContent={"center"}>
+    <Link  margin={"auto"}  textAlign={"center"} href="cadastro/form/" p={3} bgColor={"green.400"} color={"#FFFFFF"} fontWeight={1000} onClick={exibirMSG}>
       Inserir Dados
+    </Link>
+    <Link margin={"auto"}  textAlign={"center"} href="cadastro/form/" p={3} bgColor={"gray.600"} color={"#FFFFFF"} fontWeight={1000} onClick={exibirMSG}>
+      Cancelar
     </Link>
     </Box>
   </>
