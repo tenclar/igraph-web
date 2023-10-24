b65b466d58515df2104c070af1b206426f1ca6a1
Andreysson-Araujo <andreyssonaraujo@gmail.com>
2023-10-24 12:47:26 -0500

    Listagem de atendimento

--- src/pages/ad/cadastro/form/index.tsx
+++ src/pages/ad/cadastro/form/index.tsx
@@ -1,10 +1,55 @@
-import { Section } from "@/components/Section/Section";
-import { Text } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import api from "@/services/api";
+import { Text, VStack, Box } from "@chakra-ui/react";
+import { HeaderAdmin } from "@/components/Form/HeaderAdmin";
+import { format } from "date-fns";
+import { Footer } from "@/components/Form/Footer";
 
 export default function FormDados() {
-    return (
-        <>
-       
-        </>
-    )
+  const [atendimentos, setAtendimentos] = useState([]);
+
+  useEffect(() => {
+    async function fetchAtendimentos() {
+      try {
+        const response = await api.get("/atendimentos");
+        setAtendimentos(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    fetchAtendimentos();
+  }, []);
+
+  return (
+    <>
+      <HeaderAdmin />
+      <Text marginTop={"5rem"} textAlign={"center"} fontSize={"2.3rem"} fontWeight={"800"}>
+        Lista de Atendimentos
+      </Text>
+      <Box
+        margin={"AUTO"}
+        marginTop={"3rem"}
+        textAlign={"center"}
+        fontSize={"1.5rem"}
+        fontWeight={"800"}
+        w="600px" // Largura do container
+        h="400px" // Altura do container
+        borderWidth="5px"
+        borderRadius="md"
+        overflowY="scroll" // Adicione a barra de rolagem vertical
+      >
+        <VStack spacing={4} padding={4}>
+          {atendimentos.map((atendimento) => (
+            <Box key={atendimento.id}>
+              <Text>
+                {`Data: ${format(new Date(atendimento.data_de_atendimento), "dd/MM/yyyy")}, Quantidade: ${atendimento.quantidade}`}
+              </Text>
+            </Box>
+          ))}
+        </VStack>
+      </Box>
+      <Footer/>
+    </>
+  );
 }
