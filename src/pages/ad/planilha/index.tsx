f298e9e2b93acc65bd8e9bbf6cd28dfaa3e5a507
Andreysson-Araujo <andreyssonaraujo@gmail.com>
2023-12-11 13:08:26 -0500

    iniciando customização da aba planilha

--- src/pages/ad/planilha/index.tsx
+++ src/pages/ad/planilha/index.tsx
@@ -1,30 +1,73 @@
 import { Footer } from "@/components/Form/Footer";
 import { HeaderAdmin } from "@/components/Form/HeaderAdmin";
-import { Box, Button, Text, useDisclosure } from "@chakra-ui/react";
-
-
+import { Box, Button, Text, useDisclosure, Select, Input } from "@chakra-ui/react";
+import { Unidade } from "@/components/CriacaoDashboard/interfaces/UnidadeInterface";
+import { useEffect, useState } from "react";
+import api from "@/services/api";
 
 export default function GerarPlanilha() {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const [selectedUnidade, setSelectedUnidade] = useState<Unidade | null>(null);
+  const [unidades, setUnidades] = useState<Unidade[]>([]);
+
+  useEffect(() => {
+    async function fetchUnidades() {
+      try {
+        const response = await api.get("/unidades");
+        const data = response.data;
+
+        if (data && data.length > 0) {
+          setSelectedUnidade(data[0]); // Seleciona a primeira unidade como padrão
+        }
+        setUnidades(data);
+      } catch (error) {
+        console.error(error);
+        alert("Ocorreu um erro ao buscar as unidades.");
+      }
+    }
+
+    fetchUnidades(); // Chame a função fetchUnidades para buscar as unidades ao montar o componente
+  }, []); // Adicione um array vazio como segundo argumento para garantir que o useEffect execute apenas uma vez
 
   return (
     <>
       <HeaderAdmin />
       <Text marginTop={"5rem"} textAlign={"center"} fontSize={"4xl"} fontWeight="800">
-         Planilha Igraph2
+         Planilha iGraph2
       </Text>
-        <Box marginTop={"1rem"} mx="auto"  textAlign="center" fontSize="lg" fontWeight="bold" maxW="50vh" h="20vh" border=".125rem solid #000000" borderRadius="md" overflowY="scroll" backgroundColor={"gray.400"}>
-              <Text color={"#fff"} fontSize={"2xl"} fontWeight="bold">Dica:</Text>
-              <Text color={"#fff"}>Selecione um periodo de tempo e escolha os tipos de atendimentos que seram gereados na planilha do IGraph2.</Text>
-          </Box>
-      <Box mx="auto" mt={5} textAlign={"center"} fontSize="4rem" fontWeight="bold" maxW="175vh" h="60vh" overflowY="scroll">
-        <Button margin={5} w={200} h={100} backgroundColor={"green.500"} color={"#fff"} border=".125rem solid #000000" onClick={onOpen}>
-           Gerar Plainha
-        </Button>
-        <Button w={200} margin={15} h={100} backgroundColor={"blue.500"} color={"#fff"} border=".125rem solid #000000" onClick={() => window.location.href = "/ad/gerenciarUsuarios/listUser"} >
-           Visualizar Planilha
-        </Button>
+      <Box mx="auto" mt={7} textAlign={"center"} fontSize="1rem" fontWeight="bold" maxW="175vh" h="60vh" overflowY="scroll">
+        <Text>Data de Início</Text>
+        <Input
+          type={"date"}
+          textAlign={"center"}
+          maxW={200}
+          margin={"auto"}
+          bg={"#fffffff"}
+        />
+        <Text mt={3}>Data de Fim</Text>
+        <Input
+          type={"date"}
+          textAlign={"center"}
+          maxW={200}
+          margin={"auto"}
+          bg={"#fffffff"}
+        />
+        <Text fontSize={"2xl"} mt={2}>Central:</Text>
+        <Select textAlign={"center"} maxW={200} margin={"auto"} backgroundColor={"#fff"}>
+          {/* Mapeie as unidades disponíveis no Select */}
+          {unidades.map((unidade) => (
+            <option key={unidade.id} value={unidade.id}>
+              {unidade.nome}
+            </option>
+          ))}
+        </Select>
         
+        <Button margin={5} w={200} h={100} backgroundColor={"green.500"} color={"#fff"} border=".125rem solid #000000" onClick={onOpen}>
+          Gerar Planilha
+        </Button>
+        <Button w={200} margin={15} h={100} backgroundColor={"blue.500"} color={"#fff"} border=".125rem solid #000000" onClick={() => window.location.href = "/ad/gerenciarUsuarios/visualizarPlanilha"} >
+          Visualizar Planilha
+        </Button>
       </Box>
       <Footer />
     </>
